291235c5d0c98200fd6542f2b974e4c72088ce5f
Ed Palmer <edward.palmer@gmail.com>
2011-08-11 21:05:42 +0800

    Particles

--- Game/GameLayer.mm
+++ Game/GameLayer.mm
@@ -46,23 +46,66 @@
 #ifndef DRAW_BOX2D_WORLD
 
 		self.sky = [Sky skyWithTextureSize:1024];
-		[self addChild:_sky];
+		[self addChild:_sky z:kSkyLevel];
 		
 #endif
 
 		self.terrain = [Terrain terrainWithWorld:_world];
-		[self addChild:_terrain];
+		[self addChild:_terrain z:kWorldLevel];
 		
 		self.hero = [Hero heroWithGame:self];
-		[_terrain addChild:_hero];
+		[_terrain addChild:_hero z:kWorldLevel];
 
 		self.resetButton = [CCSprite spriteWithFile:@"resetButton.png"];
-		[self addChild:_resetButton];
+		[self addChild:_resetButton z:kGUILevel];
 		CGSize size = _resetButton.contentSize;
 		float padding = 8;
 		_resetButton.position = ccp(_screenW-size.width/2-padding, _screenH-size.height/2-padding);
 		
 		self.isTouchEnabled = YES;
+                    
+        CCParticleSystem *particle=[[[CCParticleSystemQuad alloc] initWithTotalParticles:200] autorelease];
+        ///////**** Assignment Texture Filename!  ****///////
+        CCTexture2D *texture=[[CCTextureCache sharedTextureCache] addImage:@"Leaf.png"];
+        particle.texture=texture;
+        particle.emissionRate=20;
+        particle.angle=180.0;
+        particle.angleVar=0.0;
+        ccBlendFunc blendFunc={GL_ONE,GL_ONE_MINUS_SRC_ALPHA};
+        particle.blendFunc=blendFunc;
+        particle.duration=-1.00;
+        particle.emitterMode=kCCParticleModeGravity;
+        ccColor4F startColor={0.15,0.29,0.20,1.00};
+        particle.startColor=startColor;
+        ccColor4F startColorVar={0.46,0.46,0.45,0.50};
+        particle.startColorVar=startColorVar;
+        ccColor4F endColor={1.00,0.30,0.26,0.00};
+        particle.endColor=endColor;
+        ccColor4F endColorVar={0.54,0.60,0.61,0.65};
+        particle.endColorVar=endColorVar;
+        particle.startSize=5.0;
+        particle.startSizeVar=5.00;
+        particle.endSize=5.0;
+        particle.endSizeVar=5.00;
+        particle.gravity=ccp(-40.00,-20.00);
+        particle.radialAccel=0.00;
+        particle.radialAccelVar=1.00;
+        particle.speed=100;
+        particle.speedVar=25;
+        particle.tangentialAccel= 0;
+        particle.tangentialAccelVar= 1;
+        particle.totalParticles=20;
+        particle.life=2.00;
+        particle.lifeVar=15.00;
+        particle.startSpin=0.00;
+        particle.startSpinVar=0.00;
+        particle.endSpin=0.00;
+        particle.endSpinVar=0.00;
+        particle.position=ccp(468.00,208.00);
+        particle.posVar=ccp(20.00,200.00);
+        
+        /////*** Assignment PARENT NODE!!!  ***/////
+        [self addChild:particle z:kParticlesLevel];
 		
 		[self scheduleUpdate];
 	}
@@ -181,7 +224,7 @@
 					  [CCFadeOut actionWithDuration:1.0f],
 					  [CCCallFuncND actionWithTarget:label selector:@selector(removeFromParentAndCleanup:) data:(void*)YES],
 					  nil]];
-	[self addChild:label];
+	[self addChild:label z:kGUILevel];
 }
 
 - (void) showFrenzy {
@@ -193,7 +236,7 @@
 					  [CCFadeOut actionWithDuration:2.0f],
 					  [CCCallFuncND actionWithTarget:label selector:@selector(removeFromParentAndCleanup:) data:(void*)YES],
 					  nil]];
-	[self addChild:label];
+	[self addChild:label z:kGUILevel];
 }
 
 - (void) showHit {
@@ -205,7 +248,7 @@
 					  [CCFadeOut actionWithDuration:1.0f],
 					  [CCCallFuncND actionWithTarget:label selector:@selector(removeFromParentAndCleanup:) data:(void*)YES],
 					  nil]];
-	[self addChild:label];
+	[self addChild:label z:kGUILevel];
 }
 
 @end
